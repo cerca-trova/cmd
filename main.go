d396d42a268519aaddd7f980d1a3a5b726540705
unknown <chenran2@sungrow.cn>
2022-07-28 21:32:33 +0800

    add version subcommand

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2022 chenran <chenran@nawaa.com>
 */
 package main
 
-import cmd "github.com/cerca-trova/cmd/cli"
+import cmd "github.com/cerca-trova/cmd/cmd"
 
 func main() {
 	cmd.Execute()
